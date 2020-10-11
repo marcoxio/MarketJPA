ef224a5ec23031c6b36f6e0eb2a312a0915c1aeb
Marco Jimenez <manjiso2@hotmail.com>
2020-10-10 20:39:36 -0500

    Implementing Purchase

---
+++ src/main/java/com/platzimarket/persistence/CompraRepository.java
@@ -0,0 +1,42 @@
+package com.platzimarket.persistence;
+
+import com.platzimarket.domain.Purchase;
+import com.platzimarket.domain.repository.PurchaseRepository;
+import com.platzimarket.persistence.crud.CompraCrudRepository;
+import com.platzimarket.persistence.entity.Compra;
+import com.platzimarket.persistence.mapper.PurchaseMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public class CompraRepository implements PurchaseRepository {
+
+    @Autowired
+    private CompraCrudRepository compraCrudRepository;
+
+    @Autowired
+    private PurchaseMapper mapper;
+
+    @Override
+    public List<Purchase> getAll() {
+        return mapper.toPurchases((List<Compra>) compraCrudRepository.findAll());
+    }
+
+    @Override
+    public Optional<List<Purchase>> getByClient(String clientId) {
+        return compraCrudRepository.findByIdCliente(clientId)
+                //Convert with mapper
+                .map(compras -> mapper.toPurchases(compras));
+    }
+
+    @Override
+    public Purchase save(Purchase purchase) {
+        Compra compra = mapper.toCompra(purchase);
+        compra.getProductos().forEach(producto -> producto.setCompra(compra));
+
+        return mapper.toPurchase(compraCrudRepository.save(compra));
+    }
+}
